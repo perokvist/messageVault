3dec2b32e325f406deab583b9d3091dd579aaff8
Slav Ivanyuk <slav.isv@gmail.com>
2015-03-19 10:20:16 -0400

    MessageWriter refactoring to make things clearer

--- src/MessageVault.Core/MessageWriter.cs
+++ src/MessageVault.Core/MessageWriter.cs
@@ -19,7 +19,7 @@ namespace MessageVault {
 		readonly int _pageSize;
 
 		readonly byte[] _buffer;
-		readonly MemoryStream _stream;
+		readonly MemoryStream _cache;
 		readonly BinaryWriter _binary;
 
 		public MessageWriter(IPageWriter pageWriter, ICheckpointWriter positionWriter) {
@@ -28,8 +28,8 @@ namespace MessageVault {
 		
 			_buffer = new byte[pageWriter.GetMaxCommitSize()];
 			_pageSize = pageWriter.GetPageSize();
-			_stream = new MemoryStream(_buffer, true);
-			_binary = new BinaryWriter(_stream, Encoding.UTF8, true);
+			_cache = new MemoryStream(_buffer, true);
+			_binary = new BinaryWriter(_cache, Encoding.UTF8, true);
 		}
 
 		public long GetPosition() {
@@ -46,59 +46,62 @@ namespace MessageVault {
 
 			//_log.Verbose("Stream {stream} at {offset}", _streamName, _position);
 
-			var tail = TailInPage(_savedPosition);
+			var tail = OffsetInLastPage(_savedPosition);
 			if (tail != 0) {
 				// preload tail
 
-				var offset = FloorInPage(_savedPosition);
+				var offset = LastFullPageTail(_savedPosition);
 				//_log.Verbose("Load tail at {offset}", offset);
 				var page = _pageWriter.ReadPage(offset);
-				_stream.Write(page, 0, tail);
+				_cache.Write(page, 0, tail);
 			}
 		}
 
-		long PageCeiling(long value) {
-			var tail = TailInPage(value);
+		long FullPagesSize(long position) {
+			var tail = OffsetInLastPage(position);
 			if (tail == 0) {
-				return value;
+				return position;
 			}
-			return value - tail + _pageSize;
+			return position - tail + _pageSize;
 		}
 
 		/// <summary>
 		/// Calculates portion of bytes in value that can fill up entire page.
 		/// </summary>
-		/// <param name="value">The value.</param>
+		/// <param name="position">The value.</param>
 		/// <returns></returns>
-		long FloorInPage(long value) {
-			return value - TailInPage(value);
+		long LastFullPageTail(long position) {
+			return position - OffsetInLastPage(position);
 		}
 
-		int TailInPage(long value) {
-			return (int) (value % _pageSize);
+		int OffsetInLastPage(long position) {
+			return (int) (position % _pageSize);
 		}
 
+		/// <summary>
+		/// Position after all data, including both saved and cached (which wasn't saved yet).
+		/// </summary>
+		/// <returns>Position of the next message to be added to the stream.</returns>
 		long VirtualPosition() {
-			return FloorInPage(_savedPosition) + _stream.Position;
+			return LastFullPageTail(_savedPosition) + _cache.Position;
 		}
 
 		long BufferStarts() {
-			return FloorInPage(_savedPosition);
+			return LastFullPageTail(_savedPosition);
 		}
 		long DataStarts() {
 			return _savedPosition;
 		}
 		long DataEnds() {
-			return BufferStarts() + _stream.Position;
+			return BufferStarts() + _cache.Position;
 		}
 		long BufferEnds() {
-			return BufferStarts() + PageCeiling(_stream.Position);
+			return BufferStarts() + FullPagesSize(_cache.Position);
 		}
 
 		void FlushBuffer() {
-			var bytesToWrite = _stream.Position;
+			var bytesToWrite = _cache.Position;
 			
-
 			//Log.Verbose("Flush Buffer {3}-[{0} ({2}) {1}]-{4}", 
 			//	DataStarts(), 
 			//	DataEnds(), 
@@ -107,17 +110,12 @@ namespace MessageVault {
 			//	BufferEnds());
 
 			// Ensure we have enough space to write everything in buffer
-			var newPosition = VirtualPosition();
-			_pageWriter.EnsureSize(PageCeiling(newPosition));
-			
-			// how many bytes do we need to write, rounded up to pages
-			var pageBytesToWrite = (int) PageCeiling(_stream.Position);
+			var newPosition = EnsureEnoughSpaceToWriteCachedMessages();
 
-			// Write all data out of buffer, including the empty tail (if present)
-			// starting from the _savedPosition, rounded down to pages
-			using (var copy = new MemoryStream(_buffer, 0, pageBytesToWrite)) {
-				_pageWriter.Save(copy, FloorInPage(_savedPosition));
-			}
+			// how many bytes do we need to write, rounded up to pages
+			var pageBytesToWrite = (int) FullPagesSize(_cache.Position);
+
+			SaveBuffer( pageBytesToWrite );
 
 			_savedPosition = newPosition;
 			// we write so little, that our page tail didn't change
@@ -126,20 +124,41 @@ namespace MessageVault {
 			}
 			// grab the tail of the stream (up to the page size)
 			// and keep it for future writes
-			var tail = TailInPage(bytesToWrite);
+			var offsetInLastSavedPage = OffsetInLastPage(bytesToWrite);
 
-			if (tail == 0) {
+			ClearCacheKeepingDataForTheLastUnfilledPage( offsetInLastSavedPage, bytesToWrite );
+		}
+
+		void ClearCacheKeepingDataForTheLastUnfilledPage( int offsetInLastSavedPage, long bytesSaved ) {
+			if( offsetInLastSavedPage == 0 ) {
 				// our position is at the page boundary, nothing to keep
-				Array.Clear(_buffer, 0, _buffer.Length);
-				_stream.Seek(0, SeekOrigin.Begin);
+//				Array.Clear( _buffer, 0, _buffer.Length );
+				_cache.Seek( 0, SeekOrigin.Begin );
 			} else {
-				// we need to save the tail in memory
-				Array.Copy(_buffer, FloorInPage(bytesToWrite), _buffer, 0, _pageSize);
-				Array.Clear(_buffer, _pageSize, _buffer.Length - _pageSize);
-				_stream.Seek(tail, SeekOrigin.Begin);
+				// we need to save the data in the last page in memory
+				// this will allow us to append data in the same page and rewrite page since need save one page at a time
+				Array.Copy( _buffer, LastFullPageTail( bytesSaved ), _buffer, 0, _pageSize ); // copy data in last not fully filled page to the start of the buffer
+//				Array.Clear( _buffer, _pageSize, _buffer.Length - _pageSize ); // clear the rest of the buffer
+				_cache.Seek( offsetInLastSavedPage, SeekOrigin.Begin );
 			}
 		}
 
+		void SaveBuffer( int pageBytesToWrite ) {
+			// Write all data out of buffer, including the empty tail (if present)
+			// starting from the _savedPosition, rounded down to pages
+			using( var copy = new MemoryStream( _buffer, 0, pageBytesToWrite ) ) {
+				// Calculate the start position of the first page that wasn't fully filled in storage
+				// This means we will overwrite a page on each save until the page gets completelly filled and only then move on to the next page
+				var firstUnfilledPagePosition = LastFullPageTail( _savedPosition );
+				_pageWriter.Save( copy, firstUnfilledPagePosition );
+			}
+		}
+
+		long EnsureEnoughSpaceToWriteCachedMessages() {
+			var newPosition = VirtualPosition();
+			_pageWriter.EnsureSize( FullPagesSize( newPosition ) );
+			return newPosition;
+		}
 
 		public long Append(ICollection<MessageToWrite> messages) {
 			if (messages.Count == 0) {
@@ -158,7 +177,7 @@ namespace MessageVault {
 
 				var sizeEstimate = MessageFormat.EstimateMessageSize(item);
 
-				var availableInBuffer = _stream.Length - _stream.Position;
+				var availableInBuffer = _cache.Length - _cache.Position;
 				if (sizeEstimate > availableInBuffer) {
 					FlushBuffer();
 				}
